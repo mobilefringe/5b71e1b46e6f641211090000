2ccef66796c30cc6dc4c43abc617c3e071584d57
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-08-14 16:43:46 -0400

    Green Valley Ranch 1.1  /contact.vue

--- contact.vue
+++ contact.vue
@@ -113,8 +113,13 @@
                         }
                     }
                     console.log(response)
-                    this.main = response[0].data
-                    this.contactInfo = response[0].data.subpages[0];
+                    if (response[0].data) {
+                        this.main = response[0].data
+                    }
+                    if (response[0].data.subpages[0]) {
+                        this.contactInfo = response[0].data.subpages[0];
+                    }
+                    
                     this.dataLoaded = true;
                 });
             },
