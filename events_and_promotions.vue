f8de3d5f18d57dc7ab2d1e1ebd8dce310c76e1b5
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-08-30 13:55:27 -0400

    Green Valley Ranch 1.0  /events_and_promotions.vue

--- events_and_promotions.vue
+++ events_and_promotions.vue
@@ -141,11 +141,12 @@
                         if (today >= showOnWebDate) {
                             var start_month = moment.tz(value.start_date, this.timezone).format("MM-YYYY");
                             if (start_month <= today_month) {
-                                value.month = moment.tz(this.timezone).format("MMMM YYYY");
+                                // value.month = moment.tz(this.timezone).format("MMMM YYYY");
+                                value.month = "Ongoing";
                                 month_heading = today_month;
                             } else {
                                 value.month = moment.tz(value.start_date, this.timezone).format("MMMM YYYY");
-                                month_heading = "Ongoing";
+                                month_heading = start_month;
                             }
 
                             if (value.store != null && value.store != undefined && _.includes(value.store.image_url, 'missing')) {
