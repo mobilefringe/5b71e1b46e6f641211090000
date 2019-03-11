afbac8c75c21b2d3eec13a697ca20c003d2430d0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-03-11 12:45:45 -0400

    Green Valley Ranch 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -141,7 +141,7 @@
                         value.zoom = 2;
                         if(!value.svgmap_region){
                             value.svgmap_region = value.id;
-                            console.log("value", value.name)
+                            console.log("no svg value", value.name)
                         }
                         else {
                             console.log("value", value.name, value.svgmap_region)
