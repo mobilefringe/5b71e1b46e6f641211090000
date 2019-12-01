7ba191ae5f0bdcda17ef860bb59dc77d64987faf
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-12-01 17:18:29 -0500

    Green Valley Ranch 1.2  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -5,7 +5,7 @@
             <div v-if="dataLoaded" v-cloak>
                 <div class="inside_page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
                     <div class="main_container position_relative">
-                        <h2>Jobs</h2>
+                        <h1>Jobs</h1>
                     </div>
                 </div>
                 <div class="main_container">
@@ -19,11 +19,9 @@
                             <div class="col-md-8">
                                 <p v-if="currentJob.jobable_type == 'Property'" class="event_store_name">{{ property.name }}</p>
                                 <p v-else class="event_store_name">{{ currentJob.store.name }}</p>
-                                <h4 class="event_name">{{ currentJob.name }}</h4>
-                                <p class="event_dates">
-                                    <span v-if="isMultiDay(currentJob)">{{ currentJob.start_date | moment("MMMM D", timezone)}} - {{ currentJob.end_date | moment("MMMM D", timezone)}}</span>
-                                    <span v-else>{{ currentJob.start_date | moment("MMMM D", timezone)}}</span>
-                                </p>
+                                <h2 class="event_name">{{ currentJob.name }}</h2>
+                                <p class="event_dates" v-if="isMultiDay(currentJob)">{{ currentJob.start_date | moment("MMMM D", timezone)}} - {{ currentJob.end_date | moment("MMMM D", timezone)}}</p>
+                                <p class="event_dates" v-else>{{ currentJob.start_date | moment("MMMM D", timezone)}}</p>
                                 <div class="event_desc event_details" v-html="currentJob.rich_description"></div>
                             </div>
                             <div class="col-md-4 hidden-sm hidden-xs">
@@ -36,7 +34,7 @@
                                     <div v-else class="no_logo_container">
                                         <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
                                         <div class="no_logo_text">
-                                            <div class="store_text"><h4>{{ currentJob.store.name }}</h4></div>
+                                            <div class="store_text"><h2>{{ currentJob.store.name }}</h2></div>
                                         </div>
                                     </div>
                                 </div>   
